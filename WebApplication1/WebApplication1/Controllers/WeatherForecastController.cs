f603379fe17572a13dd56cfcee81201cfe0f7d97
Raghuveer Addagatla <raghuveer.addagatla@gmail.com>
2020-05-08 14:00:27 -0700

    sd

--- WebApplication1/WebApplication1/Controllers/WeatherForecastController.cs
+++ WebApplication1/WebApplication1/Controllers/WeatherForecastController.cs
@@ -16,6 +16,7 @@ namespace WebApplication1.Controllers
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
+
         private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
